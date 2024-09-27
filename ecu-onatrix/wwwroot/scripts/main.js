41bb11eabce41f6696158b32770a6c52848597ee
daBard <50872455+daBard@users.noreply.github.com>
2024-09-27 16:26:55 +0200

    moved new css (with navbar toggle) to umbraco

--- ecu-onatrix/wwwroot/scripts/main.js
+++ ecu-onatrix/wwwroot/scripts/main.js
@@ -1,1 +1,9 @@
+// MOBILE NAV TOGGLE
+let navBtn = document.getElementById("nav-btn")
+let navBar = document.getElementById("nav-bar")
+
+navBtn.addEventListener('click', () => {
+    navBar.classList.toggle("active")
+})
+
 // THIS SHOULD CONTAIN JS FOR SLIDERS, CAROUSELS, LIGHT/DARK AND OTHER STUFF
